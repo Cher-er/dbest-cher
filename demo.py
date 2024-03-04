6f14835845e7f65e7afb9e6843c28af6bc9b6ab3
cher <cher18168402859@163.com>
2024-03-04 14:22:56 +0800

    update

--- demo.py
+++ demo.py
@@ -26,7 +26,7 @@ table_header = table_header[:-1]
 sql_executor.execute(f"set table_header='{table_header}'")
 
 sql_executor.execute("drop table flights_dep_delay_distance")
-sql_executor.execute(f"create table flights_dep_delay_distance(dep_delay real, distance real) from '{dataset_loc}' group by unique_carrier")
-predications = sql_executor.execute("select unique_carrier, avg(dep_delay) from flights_dep_delay_distance where 300 <= distance <= 1000 group by unique_carrier")
+sql_executor.execute(f"create table flights_dep_delay_distance(dep_delay real, distance real) from '{dataset_loc}'")
+predications = sql_executor.execute("select avg(dep_delay) from flights_dep_delay_distance where 300 <= distance <= 1000")
 
 print(predications)
