481eba3850ecf6f38cd0a7ea9f09529edf9abe73
cher <cher18168402859@163.com>
2024-03-04 10:56:33 +0800

    update

--- dbest/demo.py
+++ dbest/demo.py
@@ -1,1 +1,11 @@
 from executor.executor import SqlExecutor
+from config.config import DbestConfig
+
+
+config = DbestConfig().get_config()
+executor_parameter = config['executor_parameter']
+
+sql_executor = SqlExecutor()
+for parameter in executor_parameter:
+    print(parameter)
+    # sql_executor.execute(f"")
