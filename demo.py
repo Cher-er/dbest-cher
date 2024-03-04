6f6b598350762144b9e05c5132790b079eb4cb57
cher <cher18168402859@163.com>
2024-03-04 13:56:07 +0800

    update

--- demo.py
+++ demo.py
@@ -4,8 +4,26 @@ from config.config import DbestConfig
 
 config = DbestConfig().get_config()
 executor_parameter = config['executor_parameter']
+dataset = config['dataset']
+csv_split_char = config['csv_split_char']
+dataset_loc = config['dataset_loc']
+if dataset == "flights":
+    from schema.schema import flights
+    dataset = flights
+else:
+    print("[Error] Unknown dataset")
+    exit()
 
 sql_executor = SqlExecutor()
 for parameter in executor_parameter:
-    print(parameter)
-    # sql_executor.execute(f"")
+    sql_executor.execute(f"set {parameter}={executor_parameter[parameter]}")
+table_header = ""
+for key in dataset.keys():
+    table_header += f"{key}{csv_split_char}"
+table_header = table_header[:-1]
+sql_executor.execute(f"set table_header='{table_header}'")
+
+sql_executor.execute(f"create table flights_dep_delay_distance(dep_delay real, distance real) from '{dataset_loc}' group by unique_carrier method uniform size 0.2")
+predications = sql_executor.execute("select unique_carrier, avg(dep_delay) from flights_dep_delay_distance where 300 <= distance <= 1000 group by unique_carrier")
+
+print(predications)
---
+++ schema/schema.py
@@ -0,0 +1,14 @@
+flights = {
+    "year_date": "c",
+    "unique_carrier": "c",
+    "origin": "c",
+    "origin_state_abr": "c",
+    "dest": "c",
+    "dest_state_abr": "c",
+    "dep_delay": "n",
+    "taxi_out": "n",
+    "taxi_in": "n",
+    "arr_delay": "n",
+    "air_time": "n",
+    "distance": "n"
+}
